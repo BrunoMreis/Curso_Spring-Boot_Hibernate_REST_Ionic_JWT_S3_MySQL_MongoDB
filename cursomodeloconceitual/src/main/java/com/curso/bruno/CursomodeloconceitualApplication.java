c3e5e2fa535d3e02bc0d383a9d3673353224fddb
BrunoMreis <brbrno@hotmail.com>
2019-01-31 23:57:30 -0200

    Projeto Criado

---
+++ cursomodeloconceitual/src/main/java/com/curso/bruno/CursomodeloconceitualApplication.java
@@ -0,0 +1,14 @@
+package com.curso.bruno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomodeloconceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomodeloconceitualApplication.class, args);
+	}
+
+}
+
