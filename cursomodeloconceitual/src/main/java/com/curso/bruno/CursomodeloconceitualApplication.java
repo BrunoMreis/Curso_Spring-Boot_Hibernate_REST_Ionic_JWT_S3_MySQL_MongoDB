3774f9c3702efe538ed8781cadf6ce1e56c61663
BrunoMreis <brbrno@hotmail.com>
2019-02-05 23:22:02 -0200

    Criando operações de instanciação

--- cursomodeloconceitual/src/main/java/com/curso/bruno/CursomodeloconceitualApplication.java
+++ cursomodeloconceitual/src/main/java/com/curso/bruno/CursomodeloconceitualApplication.java
@@ -1,13 +1,33 @@
 package com.curso.bruno;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomodeloconceitualApplication {
+import com.curso.bruno.domain.Categoria;
+import com.curso.bruno.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomodeloconceitualApplication implements CommandLineRunner {
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomodeloconceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
