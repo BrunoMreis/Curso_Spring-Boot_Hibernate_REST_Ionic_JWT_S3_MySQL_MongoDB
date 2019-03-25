176d3df895f44cc1b364a15ad0b7e110b71bafb2
BrunoMreis <brbrno@hotmail.com>
2019-03-25 11:09:55 -0300

    PUT, GET e DELET para Cliente

---
+++ cursomodeloconceitual/src/main/java/com/curso/bruno/dto/ClienteDTO.java
@@ -0,0 +1,57 @@
+package com.curso.bruno.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+public class ClienteDTO {
+
+	private Integer id;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "Tamanho menor que 5 ou maior que 60 caracteres")
+	private String nome;
+
+	@Email(message = "E-mail invalido")
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String email;
+
+	public ClienteDTO() {
+	}
+
+	public ClienteDTO(Integer id, String nome, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
--- cursomodeloconceitual/src/main/java/com/curso/bruno/services/ClienteService.java
+++ cursomodeloconceitual/src/main/java/com/curso/bruno/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.curso.bruno.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.curso.bruno.domain.Cliente;
+import com.curso.bruno.dto.ClienteDTO;
 import com.curso.bruno.repositories.ClienteRepository;
+import com.curso.bruno.services.exeption.DataIntegrityException;
 import com.curso.bruno.services.exeption.ObjectNotFoundException;
 
 @Service
@@ -23,6 +30,51 @@ public class ClienteService {
 
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 
+	}
+	
+	public Cliente inset(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		update(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	
+
+	public void delete(Integer id) {
+		find(id);
+		
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir um clase relacionada");
+		}
+		
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(),objDTO.getEmail(), null , null);
+	}
+	
+	
+	private void update(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		
 	}
 
 }
