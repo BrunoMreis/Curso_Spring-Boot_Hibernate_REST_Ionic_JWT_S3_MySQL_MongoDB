9c2fb92036bfad8862a6bc5e4c2489ea9aa08a74
BrunoMreis <brbrno@hotmail.com>
2019-03-28 11:53:18 -0300

    Configurando DELETE para CLIENTE

--- cursomodeloconceitual/src/main/java/com/curso/bruno/services/ClienteService.java
+++ cursomodeloconceitual/src/main/java/com/curso/bruno/services/ClienteService.java
@@ -58,7 +58,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possivel excluir um clase relacionada");
+			throw new DataIntegrityException("Não é possivel excluir um cliente que possui pedido(s)!");
 		}
 
 	}
